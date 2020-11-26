9f74c8dceef18fb91b714d25962399132ddeca98
SyahrulSamudra <syahrulsamudra@gmail.com>
2020-11-26 13:59:02 +0700

    recycle view, intent, fragment

---
+++ sprint 2/MainActivity/app/src/main/java/com/syahrulsamudra_18102142/mainactivity/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.syahrulsamudra_18102142.mainactivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        btnLogin.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+        }
+    }
+}
