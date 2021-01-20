aa1565cb948940b90c5f9d449d1ca2550522c987
SyahrulSamudra <syahrulsamudra@gmail.com>
2021-01-20 14:21:53 +0700

    auth firebase login with veza

--- sprint 2/MainActivity/app/src/main/java/com/syahrulsamudra_18102142/mainactivity/MainActivity.kt
+++ sprint 2/MainActivity/app/src/main/java/com/syahrulsamudra_18102142/mainactivity/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.syahrulsamudra_18102142.mainactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.syahrulsamudra_18102142.mainactivity.databinding.ActivityLoginBinding
+import com.syahrulsamudra_18102142.mainactivity.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private var content: FrameLayout? = null
+
 
 
 
@@ -42,10 +49,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         val fragment = fragment_home.newInstance()
         addFragment(fragment)
+
     }
+
 }
+
